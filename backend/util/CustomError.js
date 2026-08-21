50c6f0c8736e962640e4dd183bb9f60e5d0a1fb8
retardedy0ung <thujo102@gmail.com>
2026-08-21 12:13:26 +0530

    commit

--- backend/util/CustomError.js
+++
@@ -1,8 +0,0 @@
-class CustomError extends Error{
-    constructor(StatusCode,message){
-        super()
-        this.StatusCode=StatusCode
-        this.message=message
-    }
-}
-module.exports=CustomError;
--- backend/util/wrapasync.js
+++
@@ -1,5 +0,0 @@
-module.exports=function(fn){
-return function(req,res,next){
-    fn(req,res,next).catch(next)
-}
-}
